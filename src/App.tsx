f504732da6708cc7a5a7292a979a123a785e4f10
younusraza909 <younusraza909@gmail.com>
2024-09-03 07:33:58 +0500

    Implementing Local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import HomeScreen from './screens/HomeScreen';
 
 import {NavigationContainer} from '@react-navigation/native';
@@ -8,6 +8,7 @@ import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import BookScreen from './screens/BookScreen';
 import {StackParamList} from './types';
 import BookShelvesScreen from './screens/BookShelvesScreen';
+import useBookShelves from './store';
 
 // Create a client
 const queryClient = new QueryClient();
@@ -15,6 +16,11 @@ const queryClient = new QueryClient();
 const Stack = createStackNavigator<StackParamList>();
 
 const App = () => {
+  const {loadBooksFromStorage} = useBookShelves();
+
+  useEffect(() => {
+    loadBooksFromStorage();
+  }, []);
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,33 @@
 import {create} from 'zustand';
 import {BookShelves, IBookShelf} from '../types';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type Store = {
   books: IBookShelf[];
   addBook: (bookId: string, bookShelfId: BookShelves) => void;
   updateBook: (bookId: string, bookShelfId: BookShelves) => void;
   removeBook: (bookId: string) => void;
+  loadBooksFromStorage: () => Promise<void>;
+};
+
+const syncWithLocalStorage = async (books: IBookShelf[]) => {
+  try {
+    const jsonValue = JSON.stringify(books);
+    await AsyncStorage.setItem('books', jsonValue);
+  } catch (e) {
+    console.error('Error syncing with local storage:', e);
+  }
 };
 
 const useBookShelves = create<Store>()(set => ({
   books: [],
   addBook: (bookId, bookShelfId) =>
-    set(state => ({books: [...state.books, {bookId, bookShelfId}]})),
+    set(state => {
+      const booksArr = [...state.books, {bookId, bookShelfId}];
+
+      syncWithLocalStorage(booksArr);
+      return {books: booksArr};
+    }),
   updateBook: (bookId, bookShelfId) => {
     set(state => {
       const updatedBooks = state.books.map(book => {
@@ -23,14 +39,27 @@ const useBookShelves = create<Store>()(set => ({
         }
         return book;
       });
+      syncWithLocalStorage(updatedBooks);
       return {books: updatedBooks};
     });
   },
   removeBook: bookId => {
     set(state => {
       const updatedBooks = state.books.filter(book => book.bookId !== bookId);
+      syncWithLocalStorage(updatedBooks);
       return {books: updatedBooks};
     });
+  },
+  loadBooksFromStorage: async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('books');
+      if (jsonValue != null) {
+        const books = JSON.parse(jsonValue);
+        set({books});
+      }
+    } catch (e) {
+      console.error('Error loading books from local storage:', e);
+    }
   },
 }));
 
